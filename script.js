c0c531ad25f057351659cf30ca9284ed7d979a6e
Hazik Mushtaq <hazikmushtaq130@gmail.com>
2025-05-01 18:33:33 +0530

    Updated overall UI and added Skill Section with javascript and currently working on next update for Skills section

--- script.js
+++ script.js
@@ -8,3 +8,24 @@ function opensidebar(){
 function closesidebar(){
   navbar.classList.remove('show')
 }
+
+
+
+
+document.querySelector('.Projects').addEventListener('click', function() {
+  fetch('about.html') // Load another page
+    .then(response => response.text())
+    .then(html => {
+      document.getElementById('skill').innerHTML = html;
+
+      // Wait a bit to make sure content is loaded
+      setTimeout(() => {
+        const target = document.querySelector('.projects');
+        if (target) {
+          target.scrollIntoView({ behavior: 'smooth' });
+        } else {
+          alert("Couldn't find .projects div");
+        }
+      }, 100);
+    });
+});
