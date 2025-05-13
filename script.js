c6ac22695547534b770a55a5101b1ee154cf16e4
Hazik Mushtaq <hazikmushtaq130@gmail.com>
2025-05-13 18:22:56 +0530

    Updated overall UI and added Scroll Animations, Updated navbar with new functionality

--- script.js
+++ script.js
@@ -24,8 +24,48 @@ document.querySelector('.Projects').addEventListener('click', function() {
         if (target) {
           target.scrollIntoView({ behavior: 'smooth' });
         } else {
-          alert("Couldn't find .projects div");
+          alert("Couldn't find this page (2)");
         }
       }, 100);
     });
 });
+
+
+document.addEventListener('click', function() {
+  fetch('about.html') // Load another page
+  .then(response => response.text())
+    .then(html => {
+      document.getElementById('page-body').innerHTML = html;
+
+      // Wait a bit to make sure content is loaded
+      setTimeout(() => {
+        const target = document.querySelector('.projects');
+        if (target) {
+          target.scrollIntoView({ behavior: 'smooth' });
+        } else {
+          alert("Couldn't find this page (3)");
+        }
+      }, 100);
+    });
+});
+
+document.addEventListener('click', function() {
+  fetch('about.html') // Load another page
+  .then(response => response.text())
+    .then(html => {
+      document.getElementById('mbody').innerHTML = html;
+
+      // Wait a bit to make sure content is loaded
+      setTimeout(() => {
+        const target = document.querySelector('.projects');
+        if (target) {
+          target.scrollIntoView({ behavior: 'smooth' });
+        } else {
+          alert("Couldn't find this page (4)");
+        }
+      }, 100);
+    });
+});
+
+
+
